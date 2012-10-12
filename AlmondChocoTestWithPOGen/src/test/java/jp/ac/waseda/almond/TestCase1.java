5492d00d9b75d9c999fa949f05ae4a96fb07da64
saruhei <saruhei19892046@gmail.com>
2012-10-12 18:07:59 +0900

    aoi testcases

--- AlmondChocoTestWithPOGen/src/test/java/jp/ac/waseda/almond/TestCase1.java
+++ AlmondChocoTestWithPOGen/src/test/java/jp/ac/waseda/almond/TestCase1.java
@@ -8,13 +8,18 @@ import jp.ac.waseda.almond.pages.LayoutPage;
 import jp.ac.waseda.almond.pages.NewPage;
 import jp.ac.waseda.almond.pages.ResultPage;
 import jp.ac.waseda.almond.pages.SolvePage;
+import junit.framework.Assert;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriverBackedSelenium;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
+
+import com.thoughtworks.selenium.Selenium;
 
 public class TestCase1 {
   private FirefoxDriver driver;
@@ -46,35 +51,36 @@ public class TestCase1 {
   @Test
   public void editProblem() throws InterruptedException {
     // ------ 問題編集ページヘ移動 (IndexPage) ------
-    // 最後のeditのaタグを取得（全てのeditのaタグをとってから一番最後を取る）
-    WebElement editElement = null;
-    // 取得したタグ要素をクリック
+    // 最後のeditのaタグを取得（全てのeditのaタグをとってから一番最後を取る）OK
+	List<WebElement> elementsForA_edit_List = indexPage.getElementsForA_edit();
+    WebElement editElement = elementsForA_edit_List.get(elementsForA_edit_List.size() - 1);
+    // 取得したタグ要素をクリック OK
     editElement.click();
     Thread.sleep(500);
 
     // ------ 問題の編集 (EditPage) ------
     // descriptionのTEXTAREAタグを取得
-    WebElement descriptionElement = null;
+    WebElement descriptionElement = editPage.getElementForTEXTAREA_description();
     // 2という文字列を追加入力(sendKeys)
     descriptionElement.sendKeys("2");
 
     // inputのTEXTAREAタグを取得
-    WebElement inputElement = null;
+    WebElement inputElement = editPage.getElementForTEXTAREA_input();
     // 2という文字列を追加入力(sendKeys)
     inputElement.sendKeys("2");
 
     // UpdateのBUTTONタグを取得してから、クリック(click)
-    // .click();
+    editPage.getElementForBUTTON_Update().click();
     Thread.sleep(500);
 
     // ------ トップページに戻る(LayoutPage) ------
     // トップページに戻る（layoutPageにあるロゴをclick）
-    // .click();
+    layoutPage.getElementForA_Almond_Choco().click();
     Thread.sleep(500);
 
     // ------ 問題を解くページに移動 (IndexPage) ------
     // 全てのsolveのaタグを取得
-    List<WebElement> solveElements = null;
+    List<WebElement> solveElements = indexPage.getElementsForA_solve();
     // 最後のsolveのaタグを取得
     WebElement solveElement = solveElements.get(solveElements.size() - 1);
     // 取得したタグ要素をクリック
@@ -89,16 +95,16 @@ public class TestCase1 {
   public void solveProblemWithNG() throws InterruptedException {
     // ---------------- IndexPage ----------------
     // 最後のsolveのaタグを取得（全てのsolveのaタグをとってから一番最後を取る）
-    WebElement solveElement = null;
+    WebElement solveElement = indexPage.getElementsForA_solve().get(indexPage.getElementsForA_solve().size()-1);
     // 取得したタグ要素をクリック
     solveElement.click();
     Thread.sleep(500);
 
     // ---------------- SolvePage ----------------
     // codeのTEXTAREAタグのを取得してから、"print 3"と入力(sendKeys)
-    // .sendKeys("print 4");
+    solvePage.getElementForTEXTAREA_code().sendKeys("print 4");
     // submitのBUTTONタグを取得してから、クリック(click)
-    // .click();
+    solvePage.getElementForBUTTON_Submit().click();
     Thread.sleep(500);
 
     // ---------------- 結果の確認(実験2でassertEqualsを使う) ----------------
@@ -109,23 +115,26 @@ public class TestCase1 {
   public void solveProblemWithRuby() throws InterruptedException {
     // ---------------- IndexPage ----------------
     // 最後のsolveのaタグを取得（全てのsolveのaタグをとってから一番最後を取る）
-    WebElement solveElement = null;
+    WebElement solveElement = indexPage.getElementsForA_solve().get(indexPage.getElementsForA_solve().size()-1);;
     // 取得したタグ要素をクリック
     solveElement.click();
     Thread.sleep(500);
 
     // ---------------- SolvePage ----------------
     // 言語を選ぶSELECTタグを取得
-    WebElement langElement = null;
+    WebElement langElement = solvePage.getElementForSELECT_lang_lang();
     // Rubyを選択
     new Select(langElement).selectByVisibleText("Ruby");
     // codeのTEXTAREAタグのを取得してから、"print 3"と入力(sendKeys)
-    // .sendKeys("puts 3");
+    solvePage.getElementForTEXTAREA_code().sendKeys("print 3");
     // submitのBUTTONタグを取得してから、クリック(click)
-    // .click();
+    solvePage.getElementForBUTTON_Submit().click();
     Thread.sleep(500);
 
     // ---------------- 結果の確認(実験2でassertEqualsを使う) ----------------
     // 正しく正解していることを確認
+    //Selenium selenium = new WebDriverBackedSelenium(driver, baseUrl);
+    //List<WebElement> findElements = driver.findElements(By.cssSelector("br"));
+    Assert.assertEquals(resultPage.getTextForResult_(), "OK");
   }
 }
--- AlmondChocoTestWithPOGen/src/test/java/jp/ac/waseda/almond/TestCase2WithoutPOGen.java
+++ AlmondChocoTestWithPOGen/src/test/java/jp/ac/waseda/almond/TestCase2WithoutPOGen.java
@@ -2,11 +2,18 @@ package jp.ac.waseda.almond;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriverBackedSelenium;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.thoughtworks.selenium.Selenium;
 
 public class TestCase2WithoutPOGen {
   private FirefoxDriver driver;
@@ -26,22 +33,22 @@ public class TestCase2WithoutPOGen {
   public void createNewProblem() throws InterruptedException {
     // ------ 問題作成ページヘ移動 (IndexPage) ------
     // createのaタグを取得
-    WebElement createElement = null;
+    WebElement createElement = driver.findElement(By.linkText("create"));
     // 取得したタグ要素をクリック(click)
     createElement.click();
     Thread.sleep(500);
 
     // ------ 新しい問題を作成 (NewPage) ------
     // titleのINPUTタグのを取得してから、"title"と入力(sendKeys)
-    // .sendKeys("title");
+    driver.findElement(By.name("title")).sendKeys("title");
     // descriptionのINPUTタグのを取得してから、"description"と入力(sendKeys)
-    // .sendKeys("description");
+    driver.findElement(By.name("description")).sendKeys("description");
     // inputのINPUTタグのを取得してから、"input"と入力(sendKeys)
-    // .sendKeys("input");
+    driver.findElement(By.name("input")).sendKeys("input");
     // outputのINPUTタグのを取得してから、"3"と入力(sendKeys)
-    // .sendKeys("3");
+    driver.findElement(By.name("output")).sendKeys("3");
     // submitのBUTTONタグを取得してから、クリック(click)
-    // .click();
+    driver.findElement(By.tagName("button")).click();
     Thread.sleep(500);
 
     // ---------------- 結果の確認(実験2でassertEqualsを使う) ----------------
@@ -55,16 +62,17 @@ public class TestCase2WithoutPOGen {
 
     // ------------ 問題数で確認(IndexPage) ------------
     // 問題数を数える
-    int oldProblemCount = 0; // .size();
+    int oldProblemCount = driver.findElementsByLinkText("solve").size(); // .size();
 
     // 最後のdeleteのaタグを取得（全てのdeleteのaタグをとってから一番最後を取る）
-    WebElement deleteElement = null;
-    // 取得したタグ要素をクリック
+    List<WebElement> findElementsByLinkText = driver.findElements(By.cssSelector("ul > li > a"));
+    int num = findElementsByLinkText.size() - 1;
+    WebElement deleteElement = findElementsByLinkText.get(num);    // 取得したタグ要素をクリック
     deleteElement.click();
     Thread.sleep(500);
 
     // 問題数を数える
-    int newProblemCount = 0; // .size();
+    int newProblemCount = driver.findElementsByLinkText("solve").size(); // .size();
     // 最初と比べて問題数が1減っていることを確認する（assertEquals）
     assertEquals(oldProblemCount - 1, newProblemCount);
   }
@@ -73,19 +81,28 @@ public class TestCase2WithoutPOGen {
   public void solveProblemWithOK() throws InterruptedException {
     // ---------------- IndexPage ----------------
     // 最後のsolveのaタグを取得（全てのsolveのaタグをとってから一番最後を取る）
-    WebElement solveElement = null;
+	List<WebElement> findElementsByLinkText = driver.findElements(By.cssSelector("ul > li > a"));
+	int num = findElementsByLinkText.size() - 3;
+    WebElement solveElement = findElementsByLinkText.get(num); 
     // 取得したタグ要素をクリック
     solveElement.click();
     Thread.sleep(500);
 
     // ---------------- SolvePage ----------------
     // codeのTEXTAREAタグのを取得してから、"print 3"と入力(sendKeys)
-    // .sendKeys("print 3");
+    driver.findElement(By.name("code")).sendKeys("print 3");
+    //Assert.assertEquals(driver.findElement(By.name("code")).getText(), "print 3") ;
+    
     // submitのBUTTONタグを取得してから、クリック(click)
-    // .click();
+    driver.findElement(By.tagName("button")).click();
     Thread.sleep(500);
 
     // ---------------- 結果の確認(実験2でassertEqualsを使う) ----------------
     // 正しく正解していることを確認
+    WebElement findElement = driver.findElement(By.cssSelector("html > body > p "));
+    System.out.println(findElement.getText());
+    Assert.assertTrue(findElement.getText().contains("OK"));   
+    Assert.assertTrue(findElement.getText().contains("actual = [3 ]"));   
+    Assert.assertTrue(findElement.getText().contains("expect = [3]")); 
   }
 }
